ae749f0cc203677d9792bc528bce644c737b24c6
재맵 <119744720+yookyung-lee@users.noreply.github.com>
2023-08-31 18:16:41 +0900

    a length of a string

---
+++ 편지.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(message: String): Int {
+        var answer: Int = 0
+        var length=message.length
+        
+        answer=2*length
+        return answer
+    }
+}
