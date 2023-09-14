07f9ea00950398d6c0f30bf26400a2ccf36d21d6
yookyung lee <119744720+yookyung-lee@users.noreply.github.com>
2023-09-14 17:26:10 +0900

    count

---
+++ count.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(17, 2)
+    
+    val total= greenNumbers.count() + redNumbers.count()
+    println(total)
+    
+}
